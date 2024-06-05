9c0484c0f28b46fa3643a7fa08ba9460c1a2b037
Chris O'Neil <chriso83@protonmail.com>
2024-06-05 19:04:20 +0100

    chore(release): v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.1] - 2024-06-05
+
+### 🚀 Features
+
+- Provide `dev releases aw-release-checklist` cmd
+
 ## [0.1.0] - 2024-06-03
 
 ### 🚀 Features
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='todoist-personal',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
